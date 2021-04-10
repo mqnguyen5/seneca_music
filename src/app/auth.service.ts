075f7628fd424cfbcfa2f84368a5d4f102a3c1dc
Minh Quan Nguyen <mqnguyen5@myseneca.ca>
2021-04-09 22:41:10 -0400

    remove 'tap' from auth.service.ts

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { tap } from 'rxjs/operators';
 import { environment } from './../environments/environment';
 
 import { JwtHelperService } from '@auth0/angular-jwt';
